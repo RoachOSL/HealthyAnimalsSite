d7fc9fdc4145c60885479e8ad25b66754ef786ad
Roach <os1996@o2.pl>
2023-04-30 12:41:51 +0200

    Dodanie succes page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -20,27 +20,27 @@
 
     <div class="container">
         <div class="col-md-8 mx-auto">
-            <form class="row g-3" action="https://formsubmit.co/45e01e9003d1f20987ad6f09a49af148" method="POST">
+            <form class="row g-3" @submit.prevent="submitForm" :action="submitUrl" method="POST">
                 <!-- Honeypot -->
                 <input type="text" name="_honey" style="display: none">
 
                 <!-- Disable Captcha-->
                 <input type="hidden" name="_captcha" value="false">
 
-                <input type="hidden" name="_next" value="https://roachosl.github.io/HealthyAnimalsSite/">
+                <input type="hidden" name="_next" :value="nextUrl">
 
                 <div class="col-md-5">
                     <label for="firstName">Name:</label>
-                    <input type="text" class="form-control" name="Name:" id="firstName" required>
+                    <input type="text" class="form-control" name="Name:" id="firstName" required v-model="name">
                 </div>
                 <div class="col-md-7">
                     <label for="emailInfo">Email:</label>
-                    <input type="email" class="form-control" name="E-mail:" id="emailInfo" required>
+                    <input type="email" class="form-control" name="E-mail:" id="emailInfo" required v-model="email">
                 </div>
                 <div class="col-md-12">
                     <label for="message">Message:</label>
-                    <textarea class="form-control" name="Comments,&nbsp;questions:" id="message" rows="5"
-                        required></textarea>
+                    <textarea class="form-control" name="Comments,&nbsp;questions:" id="message" rows="5" required
+                    v-model="message"></textarea>
                 </div>
                 <div class="col-md-12 text-center">
                     <button type="submit" class="btn-lg btn-primary">Submit</button>
@@ -50,6 +50,39 @@
     </div>
 </template>
 
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      submitUrl: 'https://formsubmit.co/45e01e9003d1f20987ad6f09a49af148',
+      formData: {
+        name: '',
+        email: '',
+        message: '',
+      },
+    };
+  },
+  computed: {
+    nextUrl() {
+      return "./succes"; 
+    },
+  },
+  methods: {
+    submitForm() {
+      axios.post(this.submitUrl, this.formData)
+        .then(response => {
+          console.log(response.data); 
+          window.location.href = this.nextUrl; 
+        })
+        .catch(error => {
+          console.log(error.response.data); 
+        });
+    },
+  },
+};
+</script>
 
 
 
