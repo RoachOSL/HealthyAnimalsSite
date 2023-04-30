d44cbf54b85a966456432beb7b35fc418be8e659
Roach <os1996@o2.pl>
2023-04-30 13:01:34 +0200

    update numbe 1201150

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import {createRouter, createWebHistory} from "vue-router"
 import HomeView from "../views/HomeView.vue"
 import AboutView from "../views/AboutView.vue"
 import ContactView from "../views/ContactView.vue"
-import Succes from "../views/Success.vue"
+import Success from "../views/Success.vue"
 
 
 
