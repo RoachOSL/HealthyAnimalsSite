7b1eeb2099d930d67dd3a76bcfc0c69beca710c5
Roach <os1996@o2.pl>
2023-04-30 14:32:14 +0200

    Change

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import {createRouter, createWebHistory} from "vue-router"
 import HomeView from "../views/HomeView.vue"
 import AboutView from "../views/AboutView.vue"
 import ContactView from "../views/ContactView.vue"
-import Success from "../views/Success.vue"
+
 
 
 
