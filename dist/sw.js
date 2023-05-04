7b61b5d6010c76370982d84768a9639a40fdb0f8
Roach <os1996@o2.pl>
2023-05-04 19:25:16 +0200

    update

--- dist/sw.js
+++ dist/sw.js
@@ -4,8 +4,8 @@ const assets = [
   "/src/assets/main.css",
   "/src/main.js",
   "/images/logo.jpg",
-  "/manifestLogo-192.png",
-  "/manifestLogo-512.png"
+  "/HealthyAnimalsSite/manifestLogo-192.png",
+  "/HealthyAnimalsSite/manifestLogo-512.png"
 ]
 
 self.addEventListener("install", installEvent => {
