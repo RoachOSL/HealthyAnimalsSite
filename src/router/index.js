bf65a9237eb831407281c2f3b217b919a04e4bde
Roach <os1996@o2.pl>
2023-05-04 21:15:03 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ import findVet from "../views/findVet.vue"
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
